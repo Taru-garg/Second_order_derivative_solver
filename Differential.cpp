dd2b5fa60b46f863a3700f44a04b2e449a484933
Taru <tarugarg2000@gmail.com>
2019-03-16 19:16:04 +0530

    Update Differential.cpp

--- Differential.cpp
+++ Differential.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 class differential{
 	private:
-		double x,x1,x2;
+		double x,x1,x2; //for entering values of coefficients y'' , y' and y
 		double y,y1;
 		double D;
+	public:
+		double calcD();
+		double eval();
+		void print();
+		differential();
 };
+
+
