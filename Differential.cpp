475969894a61d104152945d628d3a39b3e5e005c
Taru <tarugarg2000@gmail.com>
2019-03-16 19:35:55 +0530

    Changes

--- Differential.cpp
+++ Differential.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 
 class differential{
 	private:
 		double x,x1,x2; //for entering values of coefficients y'' , y' and y
-		double y,y1;
-		double D;
+		double y,y1; // storing values of roots
+		double D; //discriminant
 	public:
 		double calcD();
+		double calcroot();
 		double eval();
 		void print();
-		differential();
+		void initalize();
 };
-differential :: differential(){
+void differential :: initalize(){
 	double a,b,c;
 	cout<<"Enter the coefficients of y'' , y' and y";
 	cin>>a>>b>>c;
@@ -21,5 +23,15 @@ differential :: differential(){
 	x1 = b;
 	x2 = c;
 }
-
-
+double differential :: calcD(){
+	double m;
+	m = (pow(x1,2) - (4*x*x2));
+	return m;
+}
+double differential :: calcroot(){
+	double r1;
+	double r2;
+}
+int main(){
+	return 0;
+}
